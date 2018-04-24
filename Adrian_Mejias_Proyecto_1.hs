791e975fcfb745d56017f0eb3d01a91d49ada103
adrianjmejias <adrianjmejias@gmail.com>
2018-04-23 23:31:03 -0400

    mas fume

--- Adrian_Mejias_Proyecto_1.hs
+++ Adrian_Mejias_Proyecto_1.hs
@@ -15,6 +15,7 @@ peleaE 3 myHp = myHp - 330-- (1672, 110) --(hp, DMG)
 peleaE 4 myHp = myHp - 600 --(1408, 300) --(hp, DMG)
 peleaE 18 myHp = myHp - 9999 -- me mato si quiero caminar a donde he estado para hacer mas eficiente
 peleaE 0 myHp = myHp
+peleaE 6 myHp = myHp
 
 --Done and Tested
 replace_elem_in_pos (x : xs) index newVal
@@ -22,43 +23,58 @@ replace_elem_in_pos (x : xs) index newVal
         | otherwise = x : replace_elem_in_pos xs (index - 1) newVal
 
 --Done and Tested
+replace_elem_in_matrix ::(Num a) => a -> [[a]] -> Int -> Int -> [[a]]
 replace_elem_in_matrix newVal m x y = replace_elem_in_pos m y (replace_elem_in_pos (m !! y) x newVal) 
-markStep = replace_elem_in_matrix 18
 
---Done
+--Done and Tested
 peek_elem_in_matrix m x y = m !! y !! x
 
---
-checkMap xmax ymax m x y
-    | x < 0 || x > xmax = 0
-    | y < 0 || y > ymax = 0
-    | otherwise = 0--case peek_elem_in_matrix 
-    
-
-
-generar_laberinto :: ([Laberinto], Int, Int) -> [[Int]]
-generar_laberinto lab xmax ymax = gen_lab lab xmax ymax 
-    where
-        gen_lab lab xmax ymax
+check_limits max pos
+    | fst pos < 0 || fst pos >= fst max = False
+    | snd pos < 0 || snd pos >= snd max = False
+    | otherwise = True
 
 (+-+) ::Num a =>(a,a) -> (a,a) -> (a,a)
 (+-+) a b = (fst a + fst b, snd a + snd b)
 
-{-
+
 solucion_max_salud :: [[Int]] -> Int
-solucion_max_salud m = g_s m   
-where
-    g_s m = gen_sol (Punto 0 0) (length (head m)) (length m) (length (head m)) (length m)  500 -- curryfico
-    
-    gen_sol init maxx maxy goalx goalyy dmg map hp actPos = 
-        maximum[
-            (let nextPos = actPos +-+ nextDir;) solucion_max_salud
-            | nextDir <- direcciones
-            , isValid nextPos
-            ]
-            where
-                direcciones = [(1,0),(0,-1),(-1,0),(0,1)]
+solucion_max_salud m = g_s 2000 (0, 0) m where 
+    g_s = gen_sol ((length (head m)), (length m)) ((length (head m)) -1, (length m) -1)  500 --currifico
+   -- gen_sol :: (Int,Int)->(Int,Int)->Int->Int->(Int,Int)->[[Int]] ->Int
+    gen_sol max goal dmg hp actPos map = do
+        if hp <= maxSol
+            then 0
+            else 
+            maximum[
+                --Hago una lista de HPs 
+                case peeked of _
+                                | peeked == goal -> hp 
+                                | peeked == bridge -> do
+                                    let bridgeMove = nextPos +-+ nextDir
+                                    let landingPeek =  if is_valid_step bridgeMove 
+                                        then peek_elem_in_matrix m_map (fst bridgeMove) (snd bridgeMove) 
+                                        else 18 -- 18 es una casilla por la cual ya pasamos y con esto matamos el branch
+                                    case landingPeek of _
+                                        | landingPeek == bridge -> 0
+                                        | landingPeek == goal -> hp
+                                        | otherwise -> let myHP = (peleaE landingPeek hp) in if myHP <= 0 then myHP else g_s myHP bridgeMove m_map
+                                | otherwise -> let myHP = (peleaE peeked hp) in if myHP <= 0 then myHP else g_s myHP bridgeMove m_map
+                | nextDir <- direcciones
+                , let nextPos = actPos +-+ nextDir
+                , is_valid_step nextPos
+                , let peeked = peek_elem_in_matrix m (fst nextPos) (snd nextPos) 
                 
-                iSolucion laberinto = solucion_max_salud > 0
-                -}
+                ]
+            where 
+                is_valid_step = check_limits max
+                m_map = replace_elem_in_matrix 18 map (fst actPos) (snd actPos)
+                direcciones = [(1,0),(0,-1),(-1,0),(0,1)]
+                goal = 6
+                bridge = 5
+
+
+
+iSolucion laberinto m = solucion_max_salud m > 0
+                
                 
